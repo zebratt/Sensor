82193974e14f803afd707b2df447924704624841
xuejian.xu <412383619@qq.com>
2019-04-22 23:03:51 +0800

    update market

--- src/layouts/Base/Header/index.tsx
+++ src/layouts/Base/Header/index.tsx
@@ -4,7 +4,7 @@ import IconSvg from '@src/components/IconSvg'
 const Header = () => {
     return (
         <NavBar mode="light" icon={<IconSvg name="back" style={{ width: 20, height: 20 }} />}>
-            Sensor
+            <span style={{color: 'rgb(16, 142, 233)'}}>纵横期货</span>
         </NavBar>
     )
 }
--- src/pages/Market/index.tsx
+++ src/pages/Market/index.tsx
@@ -21,8 +21,21 @@ export default function Market() {
 
     return (
         <div>
+            <div styleName="row header">
+                <div styleName="name">品种合约</div>
+                <div styleName="price">最新价</div>
+                <div styleName="price">买价</div>
+                <div styleName="price">卖价</div>
+            </div>
             {contracts.map(contract => {
-                return <div key={contract.id}>{contract.contractName}</div>
+                return (
+                    <div key={contract.id} styleName="row">
+                        <div styleName="name">{contract.contractName}</div>
+                        <div styleName="price">--</div>
+                        <div styleName="price">--</div>
+                        <div styleName="price">--</div>
+                    </div>
+                )
             })}
         </div>
     )
