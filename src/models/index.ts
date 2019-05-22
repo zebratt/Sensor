b4aae2d6e41f12e6a1e87bc428dc18b6f04421af
xuejian.xu <412383619@qq.com>
2019-05-22 22:04:10 +0800

    update

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 import app from './app'
 import market from './market'
 import trade from './trade'
+import money from './money'
 
-export { app, market, trade }
+export { app, market, trade, money }
--- src/pages/Money/index.tsx
+++ src/pages/Money/index.tsx
@@ -1,13 +1,28 @@
 import './index.css'
+import { useCallback, useEffect } from 'react'
+import { useDispatch, useMappedState } from 'redux-react-hook'
+import { IRootState, IDispatch } from '@src/store'
 
 function Money() {
+    const mapState = useCallback(
+        (state: IRootState) => ({
+            body: state.money.body,
+        }),
+        []
+    )
+    const { body } = useMappedState(mapState)
+    const dispatch = useDispatch<IDispatch>()
+
+    useEffect(() => {
+        dispatch.money.fetchMoneyBody()
+    }, [])
 
     return (
         <div styleName="money">
-             <div styleName="item">
+            <div styleName="item">
                 <div styleName="label">label</div>
                 <div styleName="value">value</div>
-             </div>
+            </div>
         </div>
     )
 }
